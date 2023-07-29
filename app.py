e170b16de58a7eb2e2f0d6d85990820bc96d5bbf
Tristanchrt <chretien.tristan1@gmail.com>
2023-07-29 12:57:40 +0200

    feat: dev

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ async def start_database():
 
 @app.get("/", tags=["Root"])
 async def read_root():
-    return {"message": "Welcome to this fantastic app your are in MASTER"}
+    return {"message": "Welcome to this fantastic app your are in DEV"}
 
 
 app.include_router(AdminRouter, tags=["Administrator"], prefix="/admin")
